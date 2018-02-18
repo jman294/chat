29d8cb4785a57c3f59373dd8de7c9ae7023c1d91
Jack DuPlessis <jduplessis294@gmail.com>
2018-02-18 16:42:35 -0500

    Simple error checking server side

--- index.js
+++ index.js
@@ -4,13 +4,27 @@ const wss = new WebSocket.Server({ port: 8080 });
 let messages = [];
 let connections = [];
 
+const TEXT_T = 0;
+
 wss.on('connection', function connection(ws) {
   connections.push(ws);
   ws.on('message', function incoming(message) {
-    let json = JSON.parse(message)
-    messages.push(json.data);
-    for (ws of connections) {
-      ws.send(json.data)
+    let json, error = false
+    try {
+      json = JSON.parse(message);
+      if (!json.user || !json.data || !json.type) {
+        error = true;
+      }
+    } catch (e) {
+      error = true;
+    }
+    if (error) {
+      ws.send(JSON.stringify({error: 'Bad message'}));
+      return;
+    }
+    messages.push(json);
+    for (conn of connections) {
+      conn.send(message);
     }
   });
 });
